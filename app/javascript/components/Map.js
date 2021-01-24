988c51baab678e8e85f761ef14d23480bd6c358a
Romén Rodríguez-Gil <romen@romenrg.com>
2021-01-24 13:41:50 +0000

    Rendering a placeholder Map React component, within the home Rails view

---
+++ app/javascript/components/Map.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Map extends Component {
+  render() {
+    let className = 'map';
+    return (
+      <div className={className}>Here the Genetic Startups map will be rendered</div>
+    )
+  }
+}
+
+export default Map
