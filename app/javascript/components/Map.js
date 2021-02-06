9719d736fadfd5f133ee1869cc364feeec8ade0e
Romén Rodríguez-Gil <romen@romenrg.com>
2021-02-06 19:38:36 +0000

    Candidate layout for map using flexbox

--- app/javascript/components/Map.js
+++ app/javascript/components/Map.js
@@ -4,7 +4,24 @@ class Map extends Component {
   render() {
     let className = 'map';
     return (
-      <div className={className}>Here the Genetic Startups map will be rendered</div>
+      <div className={className}>
+        <div className="grid">
+          <div className="row">
+            <div className="col">A</div>
+            <div className="col">B</div>
+            <div className="col">C</div>
+            <div className="col">D</div>
+            <div className="col">E</div>
+          </div>
+          <div className="row">
+            <div className="col">F</div>
+            <div className="col">G</div>
+            <div className="col">H</div>
+            <div className="col">I</div>
+            <div className="col">J</div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
