24f165de5d8ffaf989bbf46e73a79e1d2c44a715
Romén Rodríguez-Gil <romen@romenrg.com>
2021-02-07 11:34:36 +0000

    Switching map layout to CSS grid
    Aport from being a more modern approach, it simplifies the HTML (no need for row divs)

--- app/javascript/components/Map.js
+++ app/javascript/components/Map.js
@@ -5,21 +5,17 @@ class Map extends Component {
     let className = 'map';
     return (
       <div className={className}>
-        <div className="grid">
-          <div className="row">
-            <div className="col">A</div>
-            <div className="col">B</div>
-            <div className="col">C</div>
-            <div className="col">D</div>
-            <div className="col">E</div>
-          </div>
-          <div className="row">
-            <div className="col">F</div>
-            <div className="col">G</div>
-            <div className="col">H</div>
-            <div className="col">I</div>
-            <div className="col">J</div>
-          </div>
+        <div className="grid-container">
+          <div className="cell">A</div>
+          <div className="cell">B</div>
+          <div className="cell">C</div>
+          <div className="cell">D</div>
+          <div className="cell">E</div>
+          <div className="cell">F</div>
+          <div className="cell">G</div>
+          <div className="cell">H</div>
+          <div className="cell">I</div>
+          <div className="cell">J</div>
         </div>
       </div>
     )
