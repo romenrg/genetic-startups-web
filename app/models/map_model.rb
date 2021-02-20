2389f19b46529f851e137c09a99b211f39b1ec86
Romén Rodríguez-Gil <romen@romenrg.com>
2021-02-20 14:16:13 +0000

    Added creation of "actionProbabilityPerQ" matrix
    Also a test to verify its correct creation

--- app/models/map_model.rb
+++ app/models/map_model.rb
@@ -1,14 +1,19 @@
 class MapModel
+  NUM_QUARTERS = 4
+  NUM_ACTIONS = 10
 
   def initialize(num_rows, num_cols)
     @num_rows = num_rows
     @num_cols = num_cols
+    @actionProbabilityPerQ = Array.new(NUM_QUARTERS){Array.new(NUM_ACTIONS)}
   end
 
   attr_reader :num_rows
   attr_reader :num_cols
+  attr_reader :actionProbabilityPerQ
 
   attr_writer :num_rows
   attr_writer :num_cols
+  attr_writer :actionProbabilityPerQ
 
 end
--- test/models/map_model_test.rb
+++ test/models/map_model_test.rb
@@ -6,4 +6,11 @@ class MapModelTest < ActiveSupport::TestCase
     assert true
   end
 
+  test "actionProbabilityPerQ matrix is generated correctly" do
+    @map = MapModel.new 3, 6
+    assert_equal MapModel::NUM_QUARTERS, @map.actionProbabilityPerQ.size
+    assert_equal MapModel::NUM_ACTIONS, @map.actionProbabilityPerQ[0].size
+  end
+
+
 end
