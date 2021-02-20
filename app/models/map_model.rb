32b56ccf53d2fb1b2018d16d740f9820bac16c80
Romén Rodríguez-Gil <romen@romenrg.com>
2021-02-20 19:34:36 +0000

    Added method to generate probabilities per quarter for each action
    Used enum for actions, for clarity. All quarter have same values for now; and the simple test added checks that total probabilities per quarter amount to 100

--- app/models/map_model.rb
+++ app/models/map_model.rb
@@ -1,19 +1,49 @@
 class MapModel
   NUM_QUARTERS = 4
   NUM_ACTIONS = 10
+  ACTIONS = {
+    "None" => 0,
+    "Advisor" => 1,
+    "Circus" => 2,
+    "Team" => 3,
+    "Product" => 4,
+    "Feedback" => 5,
+    "Investor" => 6,
+    "Doubts" => 7,
+    "Sales" => 8,
+    "BadNews" => 9
+  }
 
   def initialize(num_rows, num_cols)
     @num_rows = num_rows
     @num_cols = num_cols
-    @actionProbabilityPerQ = Array.new(NUM_QUARTERS){Array.new(NUM_ACTIONS)}
+    generateActionProbabilitiesPerQ()
   end
 
   attr_reader :num_rows
   attr_reader :num_cols
-  attr_reader :actionProbabilityPerQ
+  attr_reader :actionProbabilitiesPerQ
 
   attr_writer :num_rows
   attr_writer :num_cols
-  attr_writer :actionProbabilityPerQ
+  attr_writer :actionProbabilitiesPerQ
+
+  private
+
+  def generateActionProbabilitiesPerQ
+    @actionProbabilitiesPerQ = Array.new(NUM_QUARTERS) { Array.new(NUM_ACTIONS) }
+    for i in 0..NUM_QUARTERS-1 do
+      @actionProbabilitiesPerQ[i][ACTIONS["None"]] = 10
+      @actionProbabilitiesPerQ[i][ACTIONS["Advisor"]] = 10
+      @actionProbabilitiesPerQ[i][ACTIONS["Circus"]] = 10
+      @actionProbabilitiesPerQ[i][ACTIONS["Team"]] = 10
+      @actionProbabilitiesPerQ[i][ACTIONS["Product"]] = 10
+      @actionProbabilitiesPerQ[i][ACTIONS["Feedback"]] = 10
+      @actionProbabilitiesPerQ[i][ACTIONS["Investor"]] = 10
+      @actionProbabilitiesPerQ[i][ACTIONS["Doubts"]] = 10
+      @actionProbabilitiesPerQ[i][ACTIONS["Sales"]] = 10
+      @actionProbabilitiesPerQ[i][ACTIONS["BadNews"]] = 10
+    end
+  end
 
 end
--- test/models/map_model_test.rb
+++ test/models/map_model_test.rb
@@ -8,9 +8,26 @@ class MapModelTest < ActiveSupport::TestCase
 
   test "actionProbabilityPerQ matrix is generated correctly" do
     @map = MapModel.new 3, 6
-    assert_equal MapModel::NUM_QUARTERS, @map.actionProbabilityPerQ.size
-    assert_equal MapModel::NUM_ACTIONS, @map.actionProbabilityPerQ[0].size
+    assert_equal MapModel::NUM_QUARTERS, @map.actionProbabilitiesPerQ.size
+    assert_equal MapModel::NUM_ACTIONS, @map.actionProbabilitiesPerQ[0].size
   end
 
+  test "probabilities per map quarter should amount to 100" do
+    @map = MapModel.new 3, 6
+    @map.actionProbabilitiesPerQ.each do |quarter|
+      total_probabilities = 0
+      total_probabilities += quarter[MapModel::ACTIONS["None"]]
+      total_probabilities += quarter[MapModel::ACTIONS["Advisor"]]
+      total_probabilities += quarter[MapModel::ACTIONS["Circus"]]
+      total_probabilities += quarter[MapModel::ACTIONS["Team"]]
+      total_probabilities += quarter[MapModel::ACTIONS["Product"]]
+      total_probabilities += quarter[MapModel::ACTIONS["Feedback"]]
+      total_probabilities += quarter[MapModel::ACTIONS["Investor"]]
+      total_probabilities += quarter[MapModel::ACTIONS["Doubts"]]
+      total_probabilities += quarter[MapModel::ACTIONS["Sales"]]
+      total_probabilities += quarter[MapModel::ACTIONS["BadNews"]]
+      assert_equal 100, total_probabilities
+    end
+  end
 
 end
