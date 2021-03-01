0fa0cd50d77731fc9ed56e45d8b062c64bc6c112
Romén Rodríguez-Gil <romen@romenrg.com>
2021-03-01 23:29:30 +0000

    First test with React Testing Library
    Checking the number of cells correspond to rows*cols. Rendering the Map with the testing library, but had to use querySelector to veru¡ify the number of "cell" elements. Had to mock all image imports for jest to be able to run the test.

--- app/javascript/components/__tests__/Map.test.js
+++ app/javascript/components/__tests__/Map.test.js
@@ -1,6 +1,13 @@
+import * as React from 'react'
+import {render} from '@testing-library/react'
+import Map from "../Map";
 
 describe("Map component tests", () => {
   it("First JS test", () => {
       expect(true).toBeTruthy()
+  });
+  it("There should be a 'numRows * numCols' number of cells", () => {
+    render(<Map numRows="4" numCols="2" matrix="0 0 0 0 0 0 0 0"/>)
+    expect(document.querySelectorAll(".cell").length).toBe(8)
   });
 });
