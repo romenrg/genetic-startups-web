6d6bcdfeaa62476116a08d889a37ff365469a29a
Romén Rodríguez-Gil <romen@romenrg.com>
2021-03-29 18:48:39 +0100

    Settings panel w/ Tailwind (npm + postcss + webpacker config)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// Tailwind CSS
+import "css/application"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
