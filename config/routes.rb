5fe8054cf5cb7c9a949bc0a910224c339afcbcc8
Romén Rodríguez-Gil <romen@romenrg.com>
2021-03-30 23:19:11 +0100

    Adding "/settings" routing on the server side

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get "/info", to: 'pages#home'
+  get "/settings", to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
       namespace :v1 do
