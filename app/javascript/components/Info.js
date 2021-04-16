81711f302e85dbc147f58fa0efce2da21d77eef1
Romén Rodríguez-Gil <romen@romenrg.com>
2021-04-16 01:15:48 +0100

    Added tabs (algorithm + cell types) to info section
    Using the existing react-tabs component (possitive reviews in GitHub)

--- app/javascript/components/Info.js
+++ app/javascript/components/Info.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ACTIONS from "../algorithm/Actions";
 import {Algorithm} from "../algorithm/Algorithm";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const Info = () => {
   let actionsInfo = []
@@ -28,18 +30,37 @@ const Info = () => {
   })
   return (
     <div id="info">
-      <h2>Algorithm details</h2>
-      <p>In the map, multiple different cell types can be found. Each type represents a possible event in the life of the
-         startup. And each one can have different impacts on the startup. For that reason, each one has 10 possible values
-         and a score (the average of those possible values).</p>
-      <div className="info-grid">
-        <span className="info-item-title">Element</span>
-        <span className="info-item-title">Name</span>
-        <span className="info-item-title">Possible values</span>
-        <span className="info-item-title">Score</span>
-        <span className="info-item-title">Description</span>
-        {actionsInfo}
-      </div>
+      <Tabs>
+        <TabList>
+          <Tab>Algorithm details</Tab>
+          <Tab>Cell types</Tab>
+        </TabList>
+
+        <TabPanel>
+          <div id="alg-details" className="tab-panel">
+            <h2>Algorithm details</h2>
+            <p>This application is based on Genetic Algorithms, representing possible lives of startups.
+               The algorithm improves startup choices over generations, to achieve the most successful outcome possible;
+               in a map where investors, product launches, team members, sad news and sales, among other options, appear.</p>
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div id="cell-types" className="tab-panel">
+            <h2>Cell types</h2>
+            <p>In the map, multiple different cell types can be found. Each type represents a possible event in the life of the
+              startup. And each one can have different impacts on the startup. For that reason, each one has 10 possible values
+              and a score (the average of those possible values).</p>
+            <div className="info-grid">
+              <span className="info-item-title">Element</span>
+              <span className="info-item-title">Name</span>
+              <span className="info-item-title">Possible values</span>
+              <span className="info-item-title">Score</span>
+              <span className="info-item-title">Description</span>
+              {actionsInfo}
+            </div>
+          </div>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 };
