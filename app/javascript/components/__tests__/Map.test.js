a8729712196b824c5195e9f8890384d88b051801
Romén Rodríguez-Gil <romen@romenrg.com>
2021-05-01 20:15:53 +0100

    Fixing dependencies & JS tests
    + Adding npm test step in the CI GH Action

--- app/javascript/components/__tests__/Map.test.js
+++ app/javascript/components/__tests__/Map.test.js
@@ -1,16 +1,48 @@
-import * as React from 'react'
-import { render, fireEvent, screen } from '@testing-library/react'
+import React from 'react'
+import { render, fireEvent, screen, waitForElement } from '@testing-library/react'
 import Map from "../Map";
+import { createServer } from "miragejs"
+
+let server
+
+beforeEach(() => {
+  server = createServer()
+  server.get('/api/v1/content', () => [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0
+  ])
+})
+
+afterEach(() => {
+  server.shutdown()
+})
 
 describe("Map component tests", () => {
-  it("There should be a 'numRows * numCols' number of cells", () => {
-    render(<Map data = { { numRows:4, numCols:2, cells:[0, 0, 0, 0, 0, 0, 0, 0] } }/>)
-    expect(document.querySelectorAll(".cell").length).toBe(8)
+  it("There should be a 'numRows * numCols' number of cells", async () => {
+    // render(<Map data = { { numRows:4, numCols:2, cells:[0, 0, 0, 0, 0, 0, 0, 0] } }/>)
+    const { container } = render(<Map areSettingsShown={false} setAreSettingsShown={undefined}/>)
+    await waitForElement(() => container.querySelector('.cell'))
+    // areSettingsShown={areSettingsShown} setAreSettingsShown={setAreSettingsShown}
+    expect(document.querySelectorAll(".cell").length).toBe(128)
   });
-  it("There should be visited classes when evolution has started", () => {
-    const { map } = render(<Map data = { { numRows:4, numCols:2, cells:[0, 1, 0, 2, 0, 1, 0, 0] } }/>)
-    fireEvent.click(screen.getByText('Start Evolution'))
-    let numVisitedCells = screen.getAllByTestId("visited-cell")
-    expect(numVisitedCells.length).toBeGreaterThan(0)
+  it("There should be visited classes when evolution has started", async () => {
+    // render(<Map data = { { numRows:4, numCols:2, cells:[0, 1, 0, 2, 0, 1, 0, 0] } }/>)
+    const { container } = render(<Map areSettingsShown={false} setAreSettingsShown={undefined}/>)
+    await waitForElement(() => container.querySelector('.cell'))
+    fireEvent.click(screen.getByText('Start evolution'))
+    await waitForElement(() => screen.getAllByTestId("visited-cell"))
+    let numVisitedCells = screen.getAllByTestId("visited-cell").length
+    expect(numVisitedCells).toBeGreaterThan(0)
   });
 });
