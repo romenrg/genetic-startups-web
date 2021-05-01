4725ebb66e53b46def99f7c9ec934f7022613769
Romén Rodríguez-Gil <romen@romenrg.com>
2021-05-02 00:23:32 +0100

    React testing library tests cleanup

--- app/javascript/components/__tests__/Map.test.js
+++ app/javascript/components/__tests__/Map.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { render, fireEvent, screen, waitForElement } from '@testing-library/react'
+import { render, fireEvent, screen, waitForElement, waitFor } from '@testing-library/react'
 import Map from "../Map";
 import { createServer } from "miragejs"
 
 let server
 
-beforeEach(() => {
+beforeAll(() => {
   server = createServer()
   server.get('/api/v1/content', () => [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -24,25 +24,25 @@ beforeEach(() => {
   ])
 })
 
-afterEach(() => {
+afterAll(() => {
   server.shutdown()
 })
 
 describe("Map component tests", () => {
   it("There should be a 'numRows * numCols' number of cells", async () => {
-    // render(<Map data = { { numRows:4, numCols:2, cells:[0, 0, 0, 0, 0, 0, 0, 0] } }/>)
     const { container } = render(<Map areSettingsShown={false} setAreSettingsShown={undefined}/>)
-    await waitForElement(() => container.querySelector('.cell'))
-    // areSettingsShown={areSettingsShown} setAreSettingsShown={setAreSettingsShown}
-    expect(document.querySelectorAll(".cell").length).toBe(128)
+    await waitFor(() => {
+      expect(container.querySelectorAll(".cell").length).toBe(128)
+    })
   });
   it("There should be visited classes when evolution has started", async () => {
-    // render(<Map data = { { numRows:4, numCols:2, cells:[0, 1, 0, 2, 0, 1, 0, 0] } }/>)
     const { container } = render(<Map areSettingsShown={false} setAreSettingsShown={undefined}/>)
-    await waitForElement(() => container.querySelector('.cell'))
+    await waitFor(() => {
+      expect(container.querySelectorAll(".cell").length).toBe(128)
+    })
     fireEvent.click(screen.getByText('Start evolution'))
-    await waitForElement(() => screen.getAllByTestId("visited-cell"))
-    let numVisitedCells = screen.getAllByTestId("visited-cell").length
+    const visitedCells = await screen.findAllByTestId("visited-cell")
+    let numVisitedCells = visitedCells.length
     expect(numVisitedCells).toBeGreaterThan(0)
   });
 });
