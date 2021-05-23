74a326bd85ecabafb07679f41b12fa5ccf0e5712
Romén Rodríguez-Gil <romen@romenrg.com>
2021-05-23 13:08:48 +0100

    Adjusting test to new button text

--- app/javascript/components/__tests__/Map.test.js
+++ app/javascript/components/__tests__/Map.test.js
@@ -43,7 +43,7 @@ describe("Map component tests", () => {
     await waitFor(() => {
       expect(container.querySelectorAll(".cell").length).toBe(160)
     })
-    fireEvent.click(screen.getByText('Start evolution'))
+    fireEvent.click(screen.getByText('Run algorithm'))
     const visitedCells = await screen.findAllByTestId("visited-cell")
     let numVisitedCells = visitedCells.length
     expect(numVisitedCells).toBeGreaterThan(0)
